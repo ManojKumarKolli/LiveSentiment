016da139d598b8117f021fbe8c07db415861a91d
pujitha <akshayapujithakolli@gmail.com>
2024-01-08 21:54:57 +0530

    added logging exception

--- sentifyHub/sentifyTrackr/views.py
+++ sentifyHub/sentifyTrackr/views.py
@@ -1,81 +1,61 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from googleapiclient.discovery import build
 import os
 from dotenv import load_dotenv
-from googleapiclient.discovery import build
-import tensorflow as tf 
+import tensorflow as tf
 from tensorflow import keras
 import pickle
-# from tensorflow.python.keras.layers import TextVectorization
-# from tensorflow.python.keras.models import load_model
-# from tensorflow.python.keras.layers import Bidirectional
+import logging
 
-
+# Load environment variables
 load_dotenv()
+api_key = os.getenv('API_KEY')
+
+# YouTube API Client
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+# Global variables
+dic = {'toxic': 0, 'severe_toxic': 0, 'obscene': 0, 'threat': 0, 'insult': 0, 'identity_hate': 0, 'good': 0}
+MAX_FEATURES = 20000
+loaded_vocab = None
+
 
 with open('string_lookup_vocabulary.pkl', 'rb') as file:
-            loaded_vocab = pickle.load(file)
-# Create your views here.
-            
-dic={'toxic': 0, 'severe_toxic': 0, 'obscene': 0, 'threat': 0, 'insult': 0, 'identity_hate': 0, 'good': 0}
+    loaded_vocab = pickle.load(file)
 
 def home(request):
-    api_key = os.getenv('API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
-
-    # Build the YouTube client
-    youtube = build('youtube', 'v3', developerKey=api_key)
-
-    # Fetch comments
     response = youtube.commentThreads().list(
         part="snippet",
         videoId="5EuqYVGz_ow",
         textFormat="plainText",
-        maxResults=2  
+        maxResults=2
     ).execute()
     comments = []
     for item in response.get('items', []):
         comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
         predict(comment)
         comments.append(comment)
+    predict("I fucking hate u ")
+    comments.append("I fucking hate u ")
 
-    # Render in a template
     return render(request, 'chart.html', {'comments': comments, "dic": dic})
 
-
 def predict(comment):
-    global loaded_vocab
-    MAX_FEATURES=20000
     try:
-        #print(comment)
-        vec=keras.layers.TextVectorization(max_tokens=MAX_FEATURES,
-                                output_sequence_length=1800,
-                                output_mode='int')
+        vec = keras.layers.TextVectorization(max_tokens=MAX_FEATURES, output_sequence_length=1800, output_mode='int')
         model = keras.models.load_model('toxicity.h5')
-        print(comment)
         vec.adapt(loaded_vocab)
         vectorized_comment = vec([comment])
         results = model.predict(vectorized_comment)
-        # print(results)
         text = ''
-        columns=['toxic','severe_toxic','obscene','threat','insult','identity_hate']
-        flag=True
+        columns = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+        flag = True
         for idx, col in enumerate(columns):
-            if results[0][idx]>0.5:
-                dic[col]+=1
-                flag=False
-            text += '{}: {} '.format(col, results[0][idx]>0.5)
+            if results[0][idx] > 0.5:
+                dic[col] += 1
+                flag = False
+            text += '{}: {} '.format(col, results[0][idx] > 0.5)
         if flag:
-             dic['good']+=1
-        print(text)
-        print(dic)
-        
-
-    except Exception as e :
-        print("eyyyy",e)
-        pass
-        
-    
-    
-
-
+            dic['good'] += 1
+    except Exception as e:
+        logging.error(f"Prediction error: {e}")
