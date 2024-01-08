d7d8e556b7421704f42d4cb9d920190501061ea7
pujitha <akshayapujithakolli@gmail.com>
2024-01-08 22:05:36 +0530

    added flags to classify

--- sentifyHub/sentifyTrackr/views.py
+++ sentifyHub/sentifyTrackr/views.py
@@ -30,32 +30,38 @@ def home(request):
         textFormat="plainText",
         maxResults=2
     ).execute()
-    comments = []
-    for item in response.get('items', []):
-        comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
-        predict(comment)
-        comments.append(comment)
-    predict("I fucking hate u ")
-    comments.append("I fucking hate u ")
 
-    return render(request, 'chart.html', {'comments': comments, "dic": dic})
+    classified_comments = []
+    for item in response.get('items', []):
+        comment_text = item['snippet']['topLevelComment']['snippet']['textDisplay']
+        flags = predict(comment_text)
+        classified_comments.append({"text": comment_text, "flags": flags})
+
+    # Example test comment
+    test_comment = "I fucking hate u"
+    classified_comments.append({"text": test_comment, "flags": predict(test_comment)})
+
+    return render(request, 'chart.html', {'comments': classified_comments, "dic": dic})
+
 
 def predict(comment):
+    global dic
     try:
         vec = keras.layers.TextVectorization(max_tokens=MAX_FEATURES, output_sequence_length=1800, output_mode='int')
         model = keras.models.load_model('toxicity.h5')
         vec.adapt(loaded_vocab)
         vectorized_comment = vec([comment])
         results = model.predict(vectorized_comment)
-        text = ''
-        columns = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-        flag = True
-        for idx, col in enumerate(columns):
-            if results[0][idx] > 0.5:
-                dic[col] += 1
-                flag = False
-            text += '{}: {} '.format(col, results[0][idx] > 0.5)
-        if flag:
-            dic['good'] += 1
+
+        flags = {col: results[0][idx] > 0.5 for idx, col in enumerate(['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate'])}
+        flags['good'] = all(not flag for flag in flags.values())
+
+        # Update global counters
+        for flag, value in flags.items():
+            if value:
+                dic[flag] += 1
+
+        return flags
     except Exception as e:
         logging.error(f"Prediction error: {e}")
+        return {col: False for col in ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate', 'good']}
