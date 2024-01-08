b3f209bb74061825f08d69ed6674a22eb12c9323
pujitha <akshayapujithakolli@gmail.com>
2024-01-08 23:29:32 +0530

    views for home page added

--- sentifyHub/sentifyTrackr/views.py
+++ sentifyHub/sentifyTrackr/views.py
@@ -6,6 +6,8 @@ import tensorflow as tf
 from tensorflow import keras
 import pickle
 import logging
+from urllib.parse import urlparse, parse_qs
+
 
 # Load environment variables
 load_dotenv()
@@ -23,25 +25,47 @@ loaded_vocab = None
 with open('string_lookup_vocabulary.pkl', 'rb') as file:
     loaded_vocab = pickle.load(file)
 
+def get_video_id_from_url(url):
+    """
+    Extracts the YouTube video ID from a URL.
+    """
+    # Parsing the URL
+    parsed_url = urlparse(url)
+    if parsed_url.hostname == 'youtu.be':
+        # URL is a shortened YouTube URL, e.g., https://youtu.be/dQw4w9WgXcQ
+        return parsed_url.path[1:]
+    if parsed_url.hostname in ['www.youtube.com', 'youtube.com']:
+        # URL is a full YouTube URL
+        query_string = parse_qs(parsed_url.query)
+        video_id = query_string.get('v')
+        if video_id:
+            return video_id[0]
+    return None  # In case of an unrecognized format
+
 def home(request):
-    response = youtube.commentThreads().list(
-        part="snippet",
-        videoId="5EuqYVGz_ow",
-        textFormat="plainText",
-        maxResults=2
-    ).execute()
+    if request.method=="POST":
+        video_url=request.POST.get('video_url')
+        video_id = get_video_id_from_url(video_url)
+        response = youtube.commentThreads().list(
+            part="snippet",
+            videoId=video_id,
+            textFormat="plainText",
+            maxResults=2
+        ).execute()
 
-    classified_comments = []
-    for item in response.get('items', []):
-        comment_text = item['snippet']['topLevelComment']['snippet']['textDisplay']
-        flags = predict(comment_text)
-        classified_comments.append({"text": comment_text, "flags": flags})
+        classified_comments = []
+        for item in response.get('items', []):
+            comment_text = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            flags = predict(comment_text)
+            classified_comments.append({"text": comment_text, "flags": flags})
 
-    # Example test comment
-    test_comment = "I fucking hate u"
-    classified_comments.append({"text": test_comment, "flags": predict(test_comment)})
+        # Example test comment
+        #test_comment = "I fucking kill u"
+        #classified_comments.append({"text": test_comment, "flags": predict(test_comment)})
 
-    return render(request, 'chart.html', {'comments': classified_comments, "dic": dic})
+        return render(request, 'chart.html', {'comments': classified_comments, "dic": dic})
+    else:
+        return render(request,'home.html')
 
 
 def predict(comment):
